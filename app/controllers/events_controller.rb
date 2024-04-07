df2db355eab7fa8bdf84bc599e20384a7b446bee
riqui0130 <riquiffen8ru@icloud.com>
2024-04-07 20:01:02 +0900

    update&delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,20 +1,44 @@
 class EventsController < ApplicationController
   skip_before_action :authenticate, only: :show
 
+  def index
+    @events = Event.where("start_at > ?", Time.zone.now).order(:start_at)
+  end
+
   def new
     @event = current_user.created_events.build
   end
 
   def create
-    @event = current_user.created_events.build
-
+    @event = current_user.created_events.build(event_params)
+  
     if @event.save
       redirect_to @event, notice: "作成しました"
+    else
+      render :new, status: :unprocessable_entity
     end
   end
+  
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = current_user.created_events.find(params[:id])
+  end
+
+  def update
+    @event = current_user.created_events.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event, notice: "更新しました"
+    end
+  end
+
+  def destroy
+    @event = current_user.created_events.find(params[:id])
+    @event.destroy!
+    redirect_to root_path, notice: "削除しました"
   end
 
   private
